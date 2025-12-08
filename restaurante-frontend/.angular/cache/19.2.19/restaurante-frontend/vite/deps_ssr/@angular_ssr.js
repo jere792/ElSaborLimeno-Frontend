93c91ab93301a4a54a363b7ca20459772a52eeaf
jere792 <138648408+jere792@users.noreply.github.com>
2025-12-08 04:40:40 -0500

    catashunga 2.2

---
+++ restaurante-frontend/.angular/cache/19.2.19/restaurante-frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-CGP5HW3Q.js";
+import "./chunk-6S2ZJ7MI.js";
+import "./chunk-L46OTOIT.js";
+import "./chunk-TS2YTEFD.js";
+import "./chunk-MBWXOVFN.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
